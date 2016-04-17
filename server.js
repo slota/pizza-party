f72306b51afe66747f01df123a36df4bd9b4bbb7
John Slota <john.slota@gmail.com>
2016-04-16 21:22:15 -0600

    added check

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
