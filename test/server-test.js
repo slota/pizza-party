201cd4b4d9ce6a3e1b986044fb7857044c23d249
John Slota <john.slota@gmail.com>
2016-04-17 15:04:16 -0600

    refactored

--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const app = require('../server');
 const request = require('request');
+const fixtures = require('./fixtures');
 
 
 describe('Server', () => {
@@ -52,14 +53,9 @@ describe('Server', () => {
     });
 
     it('should receive and store data', (done) => {
-      var validPizza = {
-        pizza: {
-          name: 'A vegan pizza',
-          toppings: [ 'mushrooms', 'onions', 'garlic', 'black olives' ]
-        }
-      };
+      var payload = { pizza: fixtures.validPizza };
 
-      this.request.post('/pizzas', { form: validPizza }, (error, response) => {
+      this.request.post('/pizzas', { form: payload }, (error, response) => {
         if (error) { done(error); }
 
         var pizzaCount = Object.keys(app.locals.pizzas).length;
